ea980472974c8bd5b3933a5d1bcb1bbf47e8140f
Javier <javigo2002@gmail.com>
2022-03-17 18:47:18 +0100

    CopiaAuxiliarResultado

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 
 
-void ComportamientoJugador::actualizarVistaMapa(Sensores sensores){
-	int new_fil = fil, new_col = col;
+void ComportamientoJugador::actualizarVistaMapa(Sensores sensores,  vector< vector< unsigned char> > & mapa, int ffil, int ccol){
+	int new_fil = ffil, new_col = ccol;
 	int contador = 1, contador_ant = 0;
 
-	mapaResultado[new_fil][new_col] = sensores.terreno[0];
+	mapa[new_fil][new_col] = sensores.terreno[0];
 
 	for (int i = 0; i < 3; i++)
 		{
@@ -18,19 +18,23 @@ void ComportamientoJugador::actualizarVistaMapa(Sensores sensores){
 				case 2: new_col--; new_fil++; break;
 				case 3: new_col--; new_fil--; break;
 			}
-			
+			if (minFilAux > new_fil)
+				minFilAux = new_fil;
+			if (minColAux > new_col)
+				minColAux = new_col;
+
 			// se cuenta diferente en sur y oeste (de 3-1, 8-4, 16-9)
 			if (brujula == 2 || brujula == 3){
 				contador = contador_ant + 3+2*i;
 				contador_ant = contador;
 			}
 
+
 			for (int j = 0; j < 3+2*i; j++) {
 				if (brujula==0 || brujula == 2) // norte,sur -->columnas
-					mapaResultado[new_fil][new_col+j] = sensores.terreno[contador];
+					mapa[new_fil][new_col+j] = sensores.terreno[contador];
 				else // este oeste --> filas
-					mapaResultado[new_fil+j][new_col] = sensores.terreno[contador];
-				
+					mapa[new_fil+j][new_col] = sensores.terreno[contador];
 				
 				if (brujula == 0 || brujula == 1) //norte este
 					contador++;
@@ -40,8 +44,54 @@ void ComportamientoJugador::actualizarVistaMapa(Sensores sensores){
 		}
 }
 
-Action ComportamientoJugador::think(Sensores sensores){
 
+void ComportamientoJugador::actualizarVistaMapaAuxiliar(Sensores sensores){
+	actualizarVistaMapa(sensores, mapaAuxiliar, fil_aux, col_aux);
+}
+void ComportamientoJugador::actualizarVistaMapaResultado(Sensores sensores){
+	actualizarVistaMapa(sensores, mapaResultado, fil, col);
+}
+
+
+/*
+	PRE: SOLO VALE CUANDO NO SABEMOS DONDE EMPEZAMOS Y TODO EL MAPA EMPIEZA A '?' 
+*/
+void ComportamientoJugador::copiarAuxiliarEnResultado(vector< vector< unsigned char> > & mapaAux, vector< vector< unsigned char> > & mapaRes){
+	int fil_res=0, col_res=0;
+
+	while (mapaResultado[fil_res][col_res] == '?')
+		col_res++;
+	
+	int tamResultado = col_res;
+	col_res = 0;
+
+	mapaAux[76][90] = '0';mapaAux[76][89] = '0';mapaAux[76][88] = '0';mapaAux[76][87] = '0';
+	for (fil_res=0; fil_res < tamResultado ; fil_res++){
+		cout << endl;
+		for (int col_res = 0; col_res < tamResultado; col_res++){
+			
+			mapaRes[fil_res][col_res]= mapaAux[fil_res+minFilAux][col_res+minColAux];
+			cout << mapaRes[fil_res][col_res] << " ";
+			
+		}
+
+	}
+}
+void SacaMapa(vector< vector< unsigned char> > mapaAux, int fil, int col){
+	
+	for (size_t i = 0; i < fil; i++)
+	{
+		cout << endl;
+		for (size_t j = 0; j < col; j++)
+		{
+			cout << mapaAux[i][j]<< " ";
+		}
+		cout << endl;
+	}	
+	
+}
+
+Action ComportamientoJugador::think(Sensores sensores){
 	if (sensores.nivel > 0){
 		bien_situado=false;
 	}
@@ -50,11 +100,13 @@ Action ComportamientoJugador::think(Sensores sensores){
 	{
 		case actFORWARD:
 			switch (brujula){
-				case 0: fil--;break;
-				case 1: col++;break;
-				case 2: fil++; break;
-				case 3: col--;break;
+				case 0: fil--; fil_aux--;break;
+				case 1: col++; col_aux++;break;
+				case 2: fil++; fil_aux++;break;
+				case 3: col--; col_aux--;break;
 			}
+
+			
 			break;
 		case actTURN_L:
 			brujula=(brujula+3)%4;
@@ -72,15 +124,25 @@ Action ComportamientoJugador::think(Sensores sensores){
 		col = sensores.posC;
 		bien_situado =true;
 	}
+	
 
 	if (bien_situado){
 		
 		fil = sensores.posF;
 		col=sensores.posC;
 		brujula=sensores.sentido;
-		actualizarVistaMapa(sensores);
+		actualizarVistaMapaAuxiliar(sensores);
+		//actualizarVistaMapaResultado(sensores);
 		
 	}
+
+	if(sensores.bateria == 4670){
+			
+			cout << endl << endl << endl << endl;
+			copiarAuxiliarEnResultado(mapaAuxiliar, mapaResultado);
+			SacaMapa(mapaAuxiliar, 200, 200);
+			SacaMapa(mapaResultado, 30, 30);
+		}
 	// dECIDIR LA NUEVA ACIION
 	if ((sensores.terreno[2] == 'T' or sensores.terreno[2] == 'S' 
 		or sensores.terreno[2] == 'G') and sensores.superficie[2] == '_') {
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -10,12 +10,18 @@ class ComportamientoJugador : public Comportamiento{
     ComportamientoJugador(unsigned int size) : Comportamiento(size){
       // Constructor de la clase
       // Dar el valor inicial a las variables de estado
-      fil = col = 99;
+      fil = col = fil_aux = col_aux = 99;
+      minFilAux = minColAux=200;
+      
       brujula = 0;
       ultimaAccion = actIDLE;
       girar_derecha = false;
       bien_situado = true;
 
+      vector<unsigned char> aux(MAX_MAPA_AUX, '?');
+      for (unsigned int i = 0; i < MAX_MAPA_AUX; i++){
+         mapaAuxiliar.push_back(aux);
+      }
     }
 
     ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
@@ -23,12 +29,18 @@ class ComportamientoJugador : public Comportamiento{
 
     Action think(Sensores sensores);
     int interact(Action accion, int valor);
-    void actualizarVistaMapa(Sensores sensores);
+    void actualizarVistaMapa(Sensores sensores, vector< vector< unsigned char> > & mapa, int ffil, int ccol);
+    void actualizarVistaMapaAuxiliar(Sensores sensores);
+    void actualizarVistaMapaResultado(Sensores sensores);
+    void copiarAuxiliarEnResultado(vector< vector< unsigned char> > & mapaAux, vector< vector< unsigned char> > & mapaRes);
 
   private:
-    int fil, col, brujula;
+    int fil, col, brujula, fil_aux, col_aux, minFilAux, minColAux;
     Action ultimaAccion;
     bool girar_derecha, bien_situado;
+    const int MAX_MAPA_AUX=200, SIZE = 100;
+    vector< vector< unsigned char> > mapaAuxiliar;
+    
 
 };
 
